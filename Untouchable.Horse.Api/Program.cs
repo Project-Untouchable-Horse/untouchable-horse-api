1d400cfaf61095e7bfee25eaa310f509fa9d9cb8
Jacob Farber <farber.102@osu.edu>
2025-04-01 18:19:29 -0400

    Update OrdersController and Program.cs

--- Untouchable.Horse.Api/Program.cs
+++ Untouchable.Horse.Api/Program.cs
@@ -24,7 +24,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
