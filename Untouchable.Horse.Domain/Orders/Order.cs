d49efff758b398e4bcdcd1a5cdcbf3d8551ba936
Jacob Farber <farber.102@osu.edu>
2025-04-01 18:28:11 -0400

    Update Order.cs

--- Untouchable.Horse.Domain/Orders/Order.cs
+++ Untouchable.Horse.Domain/Orders/Order.cs
@@ -8,7 +8,7 @@ namespace Untouchable.Horse.Domain.Orders
     public class Order
     {
         public int Id { get; set; }
-        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public List<OrderItem> Items { get; set; }
         public DateTime CreatedDate { get; set; }
         public decimal TotalPrice => Items.Sum(i => i.Price);
     }
