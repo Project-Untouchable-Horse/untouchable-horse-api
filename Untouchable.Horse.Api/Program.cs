0a85136cde115d57171928eb35d31f50c606739b
RyanHernandez815 <hernandez.815@osu.edu>
2025-04-20 17:29:40 -0400

    Pages 1-15 done

--- Untouchable.Horse.Api/Program.cs
+++ Untouchable.Horse.Api/Program.cs
@@ -1,8 +1,37 @@
+using Untouchable.Horse.Api.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string authority = builder.Configuration["Auth0:Authority"] ??
+throw new ArgumentNullException("Auth0:Authority");
+
+string audience = builder.Configuration["Auth0:Audience"] ??
+throw new ArgumentNullException("Auth0:Audience");
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(options => 
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options => 
+{
+    options.Authority = authority;
+    options.Audience = audience;
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("delete:catalog", policy =>
+    policy.RequireAuthenticatedUser().RequireClaim("scope", "delete:catalog"));
+});
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -21,3 +50,9 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+app.UseAuthentication();
+
+app.UseCors();
+
+app.UseRouting();
