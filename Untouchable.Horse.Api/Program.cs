a59b24bae96e771588b61be71c2ae0e4177d86e7
Jacob Farber <farber.102@osu.edu>
2025-04-20 15:22:47 -0400

    Cors branch, update program.cs

--- Untouchable.Horse.Api/Program.cs
+++ Untouchable.Horse.Api/Program.cs
@@ -12,7 +12,6 @@ builder.Services.AddDbContext<StoreContext>(options =>
     b => b.MigrationsAssembly("Untouchable.Horse.Api"))
 );
 
-
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
